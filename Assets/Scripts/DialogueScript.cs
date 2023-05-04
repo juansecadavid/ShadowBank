65e68beec4e2964156bcd53a0473154b1b8fc2d5
mike <83485035+Mike19200@users.noreply.github.com>
2023-05-04 16:36:47 -0500

    Dialogos NPCs

--- Assets/Scripts/DialogueScript.cs
+++ Assets/Scripts/DialogueScript.cs
@@ -1,13 +1,68 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class DialogueScript : MonoBehaviour
 {
-  private bool isPlayerInRange;
+    [SerializeField] private GameObject dialogueMark;
+    [SerializeField] private GameObject dialoguePanel;
+    [SerializeField] private TMP_Text dialogueText;
+    [SerializeField, TextArea(4,6)] private string[] dialogueLines;
+
+    private float typingTime = 0.05f;
+
+    private bool isPlayerInRange;
+    private bool didDialogueStart;
+    private int lineIndex;
     void Update()
     {
-        
+        if(isPlayerInRange && Input.GetKey(KeyCode.Return))
+        {
+            if(!didDialogueStart)
+            {
+                StartDialogue();
+            }
+        }
+        /*if(isPlayerInRange && Input.GetKey(KeyCode.Return))
+        {
+           if(didDialogueStart)
+           {
+               StopDialogue();
+           }
+        }*/
+    }
+
+    private void StartDialogue()
+    {
+        didDialogueStart = true;
+        dialoguePanel.SetActive(true);
+        dialogueMark.SetActive(false);
+        lineIndex = 0;
+        Time.timeScale = 0f;
+        StartCoroutine(ShowLine());
+    }
+
+    /*private void StopDialogue()
+    {
+        didDialogueStart = false;
+        dialoguePanel.SetActive(false);
+        dialogueMark.SetActive(false);
+        lineIndex = 0;
+        Time.timeScale = 1f;
+    } */
+
+   
+
+    private IEnumerator ShowLine()
+    {
+        dialogueText.text = string.Empty;
+
+        foreach(char ch in dialogueLines[lineIndex])
+        {
+            dialogueText.text += ch;
+            yield return new WaitForSecondsRealtime(typingTime);
+        }
     }
 
     private void OnTriggerEnter2D(Collider2D collision) 
@@ -15,7 +70,7 @@ public class DialogueScript : MonoBehaviour
         if (collision.gameObject.CompareTag("Player"))
         {
             isPlayerInRange = true;
-            Debug.Log("se puede iniciar un dialogo");
+            dialogueMark.SetActive(true);
         }
     }
 
@@ -24,7 +79,7 @@ public class DialogueScript : MonoBehaviour
         if (collision.gameObject.CompareTag("Player"))
         {
             isPlayerInRange = false;
-            Debug.Log("ya no se puede iniciar un dialogo");
+            dialogueMark.SetActive(false);
         }
     }
     
