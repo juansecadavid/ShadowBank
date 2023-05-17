211aafc10dad17b1c9e42751e78349ab4f81a0f2
Juan Sebastian Cadavid Carcamo <71040945+juansecadavid@users.noreply.github.com>
2023-05-16 22:17:19 -0500

    Mejora de orden palancas

--- Assets/Scripts/TestingScripts/LeverSequence.cs
+++ Assets/Scripts/TestingScripts/LeverSequence.cs
@@ -5,15 +5,18 @@ using UnityEngine;
 public class LeverSequence : MonoBehaviour
 {
     public int neededLevers;
-    private int leverCounter;
+    public int leverCounter;
     public GameObject doorGObjct;
     private Puertas door;
-    /*
-    private List<int> orderNeccesarly = new List<int>();
+
+    private int isInOrder = 0;
+    private bool canOpen = true;
+    
+    //private List<int> orderNeccesarly = new List<int>();
 
     [SerializeField]
     InvokerEvent[] leversForThis;
-    */
+
     // Start is called before the first frame update
     void Start()
     {
@@ -35,6 +38,7 @@ public class LeverSequence : MonoBehaviour
     void Update()
     {
         OpeningDoor();
+        FixedLevers();
     }
 
     public void OnCollisionEnter2D(Collision2D collision)
@@ -48,14 +52,37 @@ public class LeverSequence : MonoBehaviour
         }*/
     }
 
-    public void ActivateLever()
+    public void ActivateLever(int orderInSequence)
     {
-        leverCounter++;
+        if (orderInSequence > isInOrder)
+        {
+            isInOrder = orderInSequence;
+            leverCounter++;
+        }
+        else
+        {
+            canOpen = false;
+            leverCounter++;
+        }
+    }
+    public void FixedLevers()
+    {
+        if (neededLevers == leverCounter)
+        {
+            isInOrder = 0;
+            canOpen = true;
+            leverCounter = 0;
+
+            for (int i = 0; i < leversForThis.Length; i++)
+            {
+                leversForThis[i].canUse=true;
+            }
+        }
     }
 
     public void OpeningDoor()
     {
-        if(neededLevers == leverCounter)
+        if (neededLevers == leverCounter && canOpen == true)
         {
             door.Abrir();
         }
